84299b554083ad9a9d7d730589a2e04a8a8f7f1d
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-07-11 19:55:06 +0900

    init

---
+++ test.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const neatFrame = require('.');
+const termSize = require('term-size');
+const test = require('tape');
+
+const contentWidth = termSize().columns - '  ┌'.length - '┐  '.length;
+
+test('neatFrame()', t => {
+  const lines = neatFrame('A\nB').split('\n');
+
+  t.equal(
+    lines.shift(),
+    `  ┌${'─'.repeat(contentWidth)}┐`,
+    'should starts with an upper frame.'
+  );
+
+  t.equal(
+    lines.shift(),
+    `  │${' '.repeat(contentWidth)}│`,
+    'should add 1 vertical padding to the top of contents.'
+  );
+
+  t.equal(
+    lines.shift(),
+    `  │ A${' '.repeat(contentWidth - ' A'.length)}│`,
+    'should add 1 horizontal padding to both ends of each line.'
+  );
+
+  t.equal(
+    lines.shift(),
+    `  │ B${' '.repeat(contentWidth - ' B'.length)}│`,
+    'should support multiline contents.'
+  );
+
+  t.equal(
+    lines.shift(),
+    `  │${' '.repeat(contentWidth)}│`,
+    'should add 1 vertical padding to the bottom of contents.'
+  );
+
+  t.equal(
+    lines.shift(),
+    `  └${'─'.repeat(contentWidth)}┘`,
+    'should starts with an lower frame.'
+  );
+
+  t.throws(
+    () => neatFrame(),
+    /^TypeError.*Expected a string to be framed with box-drawing characters, but got undefined\./,
+    'should throw an error when it takes no arguments.'
+  );
+
+  t.throws(
+    () => neatFrame(Buffer.from('a')),
+    /TypeError.*Expected a string to be framed with box-drawing characters, but got <Buffer 61>\./,
+    'should throw an error when it takes a non-string argument.'
+  );
+
+  t.end();
+});
